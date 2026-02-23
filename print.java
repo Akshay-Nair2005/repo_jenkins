b0eb6aee96358ad96a3a7aa183391035065faa9e
Nair Akshay Anilkumar <115722931+Akshay-Nair2005@users.noreply.github.com>
2026-02-23 12:33:58 +0530

    Update print.java

--- print.java
+++ print.java
@@ -2,6 +2,7 @@ public class print{
     public static void main(String[] args){
         System.out.print("Heyyy");
         System.out.print("Hiiiiiiii");
+        System.out.print("Hiiiiiiii");
     }
 }
 
