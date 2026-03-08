2a1edd42314570e60ae13f62e706ce52bec1d71a
Nair Akshay Anilkumar <115722931+Akshay-Nair2005@users.noreply.github.com>
2026-03-08 15:38:13 +0530

    Add Hello.java with main method and print statements

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Heyyy");
+        System.out.println("Hiiiiiiii");
+        System.out.println("Hiiiiiiii");
+        System.out.println("Heyyy")
+    }
+}
